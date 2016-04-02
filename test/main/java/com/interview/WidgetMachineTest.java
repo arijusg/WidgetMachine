8900a7d7b96b903b5cbcc1de5aaa1b27238af4a6
Arijus Gilbrantas <Arijus Gilbrantas>
2016-04-02 18:32:34 +0100

    Added tests for WidgetMachine checking for half/full/onaAndAHalfbatches

--- test/main/java/com/interview/WidgetMachineTest.java
+++ test/main/java/com/interview/WidgetMachineTest.java
@@ -10,26 +10,98 @@ public class WidgetMachineTest {
 
     //Different types of engines will be tested in integration tests
     @Test
-    public void engine(@Injectable final IEngine engine1,
-                       @Injectable final IEngineAssembly engineAssembly) {
+    public void produceHalfBatchUsingPetrolEngine(@Injectable final IEngine engine,
+                                                  @Injectable final IEngineAssembly engineAssembly) {
 
         new Expectations() {{
-            engine1.getFuelType();
+            engine.getFuelType();
             result = FuelType.PETROL;
-            engine1.getBatchSize();
+            engine.getBatchSize();
             result = 8;
-            engine1.getBatchCost();
+            engine.getBatchCost();
             result = 9.00;
 
             engineAssembly.getEngine();
-            result = engine1;
+            result = engine;
             engineAssembly.getIsRunning();
             result = true;
             times = 1;
         }};
 
         WidgetMachine wm = new WidgetMachine(engineAssembly);
-        double cost = wm.produceWidgets(10);
+        double cost = wm.produceWidgets(4);
+
+        assertEquals(9.00, cost, 0.01);
+
+        new VerificationsInOrder() {{
+            engineAssembly.fillTank(FuelType.PETROL, 100);
+            times = 1;
+            engineAssembly.start();
+            times = 1;
+            engineAssembly.getIsRunning();
+            times = 1;
+            engineAssembly.stop();
+            times = 1;
+        }};
+    }
+
+    @Test
+    public void produceOneBatchUsingPetrolEngine(@Injectable final IEngine engine,
+                                                 @Injectable final IEngineAssembly engineAssembly) {
+
+        new Expectations() {{
+            engine.getFuelType();
+            result = FuelType.PETROL;
+            engine.getBatchSize();
+            result = 8;
+            engine.getBatchCost();
+            result = 9.00;
+
+            engineAssembly.getEngine();
+            result = engine;
+            engineAssembly.getIsRunning();
+            result = true;
+            times = 1;
+        }};
+
+        WidgetMachine wm = new WidgetMachine(engineAssembly);
+        double cost = wm.produceWidgets(8);
+
+        assertEquals(9.00, cost, 0.01);
+
+        new VerificationsInOrder() {{
+            engineAssembly.fillTank(FuelType.PETROL, 100);
+            times = 1;
+            engineAssembly.start();
+            times = 1;
+            engineAssembly.getIsRunning();
+            times = 1;
+            engineAssembly.stop();
+            times = 1;
+        }};
+    }
+
+    @Test
+    public void produceOneAndAHalfBatchesUsingPetrolEngine(@Injectable final IEngine engine,
+                                                           @Injectable final IEngineAssembly engineAssembly) {
+
+        new Expectations() {{
+            engine.getFuelType();
+            result = FuelType.PETROL;
+            engine.getBatchSize();
+            result = 8;
+            engine.getBatchCost();
+            result = 9.00;
+
+            engineAssembly.getEngine();
+            result = engine;
+            engineAssembly.getIsRunning();
+            result = true;
+            times = 1;
+        }};
+
+        WidgetMachine wm = new WidgetMachine(engineAssembly);
+        double cost = wm.produceWidgets(12);
 
         assertEquals(18, cost, 0.01);
 
@@ -44,8 +116,4 @@ public class WidgetMachineTest {
             times = 1;
         }};
     }
-    //Create widgets using half a batch
-    //Create widgets using full batch
-    //Create widgets using one and a half batch
-
 }
