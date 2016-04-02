699234b2951d9f4083a15e6447815c75c2ab9403
Arijus Gilbrantas <Arijus Gilbrantas>
2016-04-02 12:59:58 +0100

    Template test is working! Inifinitest and auto compile on save

---
+++ test/main/java/com/interview/WidgetMachineTest.java
@@ -0,0 +1,16 @@
+package main.java.com.interview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ariju on 02/04/2016.
+ */
+public class WidgetMachineTest {
+    @Test
+    public void TestTemplate() {
+assertEquals(1 ,1);
+    }
+
+}
